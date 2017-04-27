0fcae95e7d577c17fdbee2603e716aeacfed099b
Gabriel <gabco96@hotmail.com>
2017-04-27 16:38:40 +0200

    app.js modified

--- app.js
+++ app.js
@@ -7,11 +7,11 @@
 
 var express = require('express');
 var app = express();
-require('dotenv').config({path:'./.env.example'});
+// require('dotenv').config({ path: './.env.example' });
 
 //check
-console.log(process.env.PW);
-console.log(process.env.EMAIL);
+// console.log(process.env.PW);
+// console.log(process.env.EMAIL);
 
 
 
@@ -19,17 +19,22 @@ console.log(process.env.EMAIL);
 var whiskyExchangeApi = require('./mods/whiskyex.js');
 var maltApi = require('./mods/malt.js');
 var whiskyFr = require('./mods/whiskyfr.js');
-function setupScript(){
-	console.log("Setting up script");
-	maltApi.getTheMasterOfMalt();
-	whiskyExchangeApi.getTheWhiskyExchange();
-	whiskyFr.setupWhiskyFr();
-	setInterval(maltApi.getTheMasterOfMalt,300000);
-	setInterval(whiskyExchangeApi.getTheWhiskyExchange,300000);
-	setInterval(whiskyFr.getWhiskyFr,300000);
+
+function setupScript() {
+    console.log("Setting up script");
+    maltApi.getTheMasterOfMalt();
+    whiskyExchangeApi.getTheWhiskyExchange();
+    whiskyFr.setupWhiskyFr();
+    setInterval(maltApi.getTheMasterOfMalt, 300000);
+    setInterval(whiskyExchangeApi.getTheWhiskyExchange, 300000);
+    setInterval(whiskyFr.getWhiskyFr, 300000);
 }
 
-app.listen(8080,function(){
-	console.log("Server listening on localhost:8080");
-	setupScript();
+app.get('/', function(req, res) {
+    res.send('<h2>Some cool automated script running over here</h2>');
+
+});
+app.listen(8080, function() {
+    console.log("Server listening on localhost:8080");
+    setupScript();
 });
