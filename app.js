481aac9ef90264529e995e90412d4003689b72ec
GabrielBuragev <gabriel.buragev96@gmail.com>
2019-12-18 15:20:29 +0100

    Remove dotenv

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
  *   Author : Gabriel Buragev - gabco96[at]hotmail.com
  */
 
-require('dotenv').config();
-var express = require('express');
-var https = require('https');
+var express = require("express");
+var https = require("https");
 var app = express();
 
-var whiskyExchange = require('./libs/whiskyex.js');
-var Malt = require('./libs/malt.js');
-var whiskyFr = require('./libs/whiskyfr.js');
-var LionsWhisky = require('./libs/lionsWhisky/Scraper');
-var NickollSandPerks = require('./libs/nickollsandperks');
+var whiskyExchange = require("./libs/whiskyex.js");
+var Malt = require("./libs/malt.js");
+var whiskyFr = require("./libs/whiskyfr.js");
+var LionsWhisky = require("./libs/lionsWhisky/Scraper");
+var NickollSandPerks = require("./libs/nickollsandperks");
 // TODO: Heal this script
-var VinMonopolet = require('./libs/vinmonopolet');
-var oldWhisky = require('./libs/oldWhisky/Scraper');
+var VinMonopolet = require("./libs/vinmonopolet");
+var oldWhisky = require("./libs/oldWhisky/Scraper");
 
 // DDOS PROTECTED SITES
 // var RoyalMileWhiskies = require("./libs/royalmileWhisky/Scraper");
@@ -23,48 +22,48 @@ var oldWhisky = require('./libs/oldWhisky/Scraper');
 // var WhiskyDe = require('./libs/whisky.de');
 
 function setupScript() {
-    console.log("Setting up script");
+  console.log("Setting up script");
 
-    Malt.getTheMasterOfMalt();
-    whiskyExchange.getTheWhiskyExchange();
-    whiskyFr.setupWhiskyFr();
-    setInterval(Malt.getTheMasterOfMalt, 300000);
-    setInterval(whiskyExchange.getTheWhiskyExchange, 300000);
-    setInterval(whiskyFr.getWhiskyFr, 300000);
+  Malt.getTheMasterOfMalt();
+  whiskyExchange.getTheWhiskyExchange();
+  whiskyFr.setupWhiskyFr();
+  setInterval(Malt.getTheMasterOfMalt, 300000);
+  setInterval(whiskyExchange.getTheWhiskyExchange, 300000);
+  setInterval(whiskyFr.getWhiskyFr, 300000);
 }
 
 function testMailer() {
-    var mailer = require('./libs/mailer.js');
-    mailer.testMailFunction();
+  var mailer = require("./libs/mailer.js");
+  mailer.testMailFunction();
 }
 testMailer();
 
-
 // Loopback request to prevent app sleeping!
 var doLbRequest = function() {
-    https.get('https://whiskyscraper.herokuapp.com/', function(res) {
-        var rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-                console.log("RESPONE FROM LOOPBACK REQUEST : " + rawData);
-            } catch (e) {
-                console.error(e.message);
-            }
-        });
+  https.get("https://whiskyscraper.herokuapp.com/", function(res) {
+    var rawData = "";
+    res.on("data", chunk => {
+      rawData += chunk;
     });
-}
+    res.on("end", () => {
+      try {
+        console.log("RESPONE FROM LOOPBACK REQUEST : " + rawData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  });
+};
 setInterval(function() {
-    doLbRequest();
+  doLbRequest();
 }, 300000); // every 5 minutes (300000)
 doLbRequest();
 //  -   -   -   -
 
-app.get('/', function(req, res) {
-    res.send('<h2>Some cool automated script running over here</h2>');
-
+app.get("/", function(req, res) {
+  res.send("<h2>Some cool automated script running over here</h2>");
 });
 var server = app.listen(process.env.PORT || 5000, function() {
-    console.log("Server listening on localhost:" + server.address().port);
-    setupScript();
+  console.log("Server listening on localhost:" + server.address().port);
+  setupScript();
 });
